d5f38e6adc4e3f06c6da3373e6c66270bff6358b
Jin <it.minhhung@gmail.com>
2024-09-21 22:46:34 +0700

    change style

--- script.js
+++ script.js
@@ -13,95 +13,95 @@ $(document).ready(function(){
     ];
 
     vowelItems.forEach((v, idx) =>{
-        $(".vowel-menu").append(`<span class="vowel">${v}</span>`);
+        $("#filter-buttons .col-12").append(`<button class="btn mb-2 mx-1" data-filter="${v}">${v}</button>`);
     });
     
     const wordBank = [
-        {vowelNbr: 'ɑ:', word: "not", phonetic: "nɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "hot", phonetic: "hɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "lot", phonetic: "lɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "dot", phonetic: "dɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "lock", phonetic: "lɑ:k", end: "ck"},
-        {vowelNbr: 'ɑ:', word: "talk", phonetic: "tɑ:k", end: "lk"},
-        {vowelNbr: 'ɑ:', word: "taught", phonetic: "tɑ:t", end: "ght"},
-        {vowelNbr: 'ɑ:', word: "tall", phonetic: "tɑ:l", end: "ll"},
-        {vowelNbr: 'ɑ:', word: "cause", phonetic: "kɑ:z", end: "se"},
-        {vowelNbr: 'ɑ:', word: "fought", phonetic: "fɑ:t", end: "ght"},
-        {vowelNbr: 'ɑ:', word: "bought", phonetic: "bɑ:t", end: "ght"},
-        {vowelNbr: 'ɑ:', word: "what", phonetic: "wɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "wall", phonetic: "wɑ:l", end: "ll"},
-        {vowelNbr: 'ɑ:', word: "car", phonetic: "kɑ:r", end: "r"},
-        {vowelNbr: 'ɑ:', word: "guard", phonetic: "gɑ:rd", end: "rd"},
-        {vowelNbr: 'ɑ:', word: "fault", phonetic: "fɑ:lt", end: "lt"},
-        {vowelNbr: 'ɑ:', word: "card", phonetic: "cɑ:rd", end: "rd"},
-        {vowelNbr: 'ɑ:', word: "carve", phonetic: "kɑ:rv", end: "rve"},
-        {vowelNbr: 'ɑ:', word: "dark", phonetic: "dɑ:rk", end: "rk"},
-        {vowelNbr: 'ɑ:', word: "dart", phonetic: "dɑ:rt", end: "rt"},
-        {vowelNbr: 'ɑ:', word: "part", phonetic: "pɑ:rt", end: "rt"},
-        {vowelNbr: 'ɑ:', word: "park", phonetic: "pɑ:rk", end: "rk"},
-        {vowelNbr: 'ɑ:', word: "bark", phonetic: "bɑ:rk", end: "rk"},
-        {vowelNbr: 'ɑ:', word: "heart", phonetic: "hɑ:rt", end: "rt"},
-        {vowelNbr: 'ɑ:', word: "hard", phonetic: "hɑ:rd", end: "rd"},
-        {vowelNbr: 'ɑ:', word: "harm", phonetic: "hɑ:rm", end: "rm"},
-        {vowelNbr: 'ɑ:', word: "mart", phonetic: "mɑ:rt", end: "rt"},
-        {vowelNbr: 'ɑ:', word: "farm", phonetic: "fɑ:rm", end: "rm"},
-        {vowelNbr: 'ɑ:', word: "stop", phonetic: "stɑ:p", end: "p"},
-        {vowelNbr: 'ɑ:', word: "star", phonetic: "stɑ:r", end: "r"},
-        {vowelNbr: 'ɑ:', word: "start", phonetic: "stɑ:rt", end: "rt"},
-        {vowelNbr: 'ɑ:', word: "block", phonetic: "blɑ:k", end: "ck"},
-        {vowelNbr: 'ɑ:', word: "brought", phonetic: "brɑ:t", end: "ght"},
-        {vowelNbr: 'ɑ:', word: "drop", phonetic: "drɑ:p", end: "p"},
-        {vowelNbr: 'ɑ:', word: "small", phonetic: "smɑ:l", end: "ll"},
-        {vowelNbr: 'ɑ:', word: "stock", phonetic: "stɑ:k", end: "ck"},
-        {vowelNbr: 'ɑ:', word: "spot", phonetic: "spɑ:t", end: "t"},
-        {vowelNbr: 'ɑ:', word: "clock", phonetic: "klɑ:k", end: "ck"},
-        {vowelNbr: 'ɑ:', word: "floss", phonetic: "flɑ:s", end: "ss"},
-        {vowelNbr: 'ɑ:', word: "blonde", phonetic: "blɑ:nd", end: "nde"},
-        {vowelNbr: 'ɑ:', word: "strong", phonetic: "strɑ:ŋ", end: "ng"},
+        {vowel: 'ɑ:', word: "not", phonetic: "nɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "hot", phonetic: "hɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "lot", phonetic: "lɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "dot", phonetic: "dɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "lock", phonetic: "lɑ:k", end: "ck"},
+        {vowel: 'ɑ:', word: "talk", phonetic: "tɑ:k", end: "lk"},
+        {vowel: 'ɑ:', word: "taught", phonetic: "tɑ:t", end: "ght"},
+        {vowel: 'ɑ:', word: "tall", phonetic: "tɑ:l", end: "ll"},
+        {vowel: 'ɑ:', word: "cause", phonetic: "kɑ:z", end: "se"},
+        {vowel: 'ɑ:', word: "fought", phonetic: "fɑ:t", end: "ght"},
+        {vowel: 'ɑ:', word: "bought", phonetic: "bɑ:t", end: "ght"},
+        {vowel: 'ɑ:', word: "what", phonetic: "wɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "wall", phonetic: "wɑ:l", end: "ll"},
+        {vowel: 'ɑ:', word: "car", phonetic: "kɑ:r", end: "r"},
+        {vowel: 'ɑ:', word: "guard", phonetic: "gɑ:rd", end: "rd"},
+        {vowel: 'ɑ:', word: "fault", phonetic: "fɑ:lt", end: "lt"},
+        {vowel: 'ɑ:', word: "card", phonetic: "cɑ:rd", end: "rd"},
+        {vowel: 'ɑ:', word: "carve", phonetic: "kɑ:rv", end: "rve"},
+        {vowel: 'ɑ:', word: "dark", phonetic: "dɑ:rk", end: "rk"},
+        {vowel: 'ɑ:', word: "dart", phonetic: "dɑ:rt", end: "rt"},
+        {vowel: 'ɑ:', word: "part", phonetic: "pɑ:rt", end: "rt"},
+        {vowel: 'ɑ:', word: "park", phonetic: "pɑ:rk", end: "rk"},
+        {vowel: 'ɑ:', word: "bark", phonetic: "bɑ:rk", end: "rk"},
+        {vowel: 'ɑ:', word: "heart", phonetic: "hɑ:rt", end: "rt"},
+        {vowel: 'ɑ:', word: "hard", phonetic: "hɑ:rd", end: "rd"},
+        {vowel: 'ɑ:', word: "harm", phonetic: "hɑ:rm", end: "rm"},
+        {vowel: 'ɑ:', word: "mart", phonetic: "mɑ:rt", end: "rt"},
+        {vowel: 'ɑ:', word: "farm", phonetic: "fɑ:rm", end: "rm"},
+        {vowel: 'ɑ:', word: "stop", phonetic: "stɑ:p", end: "p"},
+        {vowel: 'ɑ:', word: "star", phonetic: "stɑ:r", end: "r"},
+        {vowel: 'ɑ:', word: "start", phonetic: "stɑ:rt", end: "rt"},
+        {vowel: 'ɑ:', word: "block", phonetic: "blɑ:k", end: "ck"},
+        {vowel: 'ɑ:', word: "brought", phonetic: "brɑ:t", end: "ght"},
+        {vowel: 'ɑ:', word: "drop", phonetic: "drɑ:p", end: "p"},
+        {vowel: 'ɑ:', word: "small", phonetic: "smɑ:l", end: "ll"},
+        {vowel: 'ɑ:', word: "stock", phonetic: "stɑ:k", end: "ck"},
+        {vowel: 'ɑ:', word: "spot", phonetic: "spɑ:t", end: "t"},
+        {vowel: 'ɑ:', word: "clock", phonetic: "klɑ:k", end: "ck"},
+        {vowel: 'ɑ:', word: "floss", phonetic: "flɑ:s", end: "ss"},
+        {vowel: 'ɑ:', word: "blonde", phonetic: "blɑ:nd", end: "nde"},
+        {vowel: 'ɑ:', word: "strong", phonetic: "strɑ:ŋ", end: "ng"},
 
-        {vowelNbr: 'e', word: "air", phonetic: "er", end: "r"},
-        {vowelNbr: 'e', word: "heir", phonetic: "er", end: "r"},
-        {vowelNbr: 'e', word: "hair", phonetic: "her", end: "r"},
-        {vowelNbr: 'e', word: "fair", phonetic: "fer", end: "r"},
-        {vowelNbr: 'e', word: "pair", phonetic: "per", end: "r"},
-        {vowelNbr: 'e', word: "bear", phonetic: "ber", end: "r"},
-        {vowelNbr: 'e', word: "care", phonetic: "ker", end: "re"},
-        {vowelNbr: 'e', word: "where", phonetic: "wer", end: "re"},
-        {vowelNbr: 'e', word: "share", phonetic: "ʃer", end: "re"},
-        {vowelNbr: 'e', word: "let", phonetic: "let", end: "t"},
-        {vowelNbr: 'e', word: "less", phonetic: "les", end: "s"},
-        {vowelNbr: 'e', word: "dead", phonetic: "ded", end: "d"},
-        {vowelNbr: 'e', word: "death", phonetic: "deθ", end: "th"},
-        {vowelNbr: 'e', word: "deaf", phonetic: "def", end: "f"},
-        {vowelNbr: 'e', word: "get", phonetic: "get", end: "t"},
-        {vowelNbr: 'e', word: "guess", phonetic: "ges", end: "ss"},
-        {vowelNbr: 'e', word: "pen", phonetic: "pen", end: "n"},
-        {vowelNbr: 'e', word: "bell", phonetic: "bel", end: "ll"},
-        {vowelNbr: 'e', word: "sell", phonetic: "sel", end: "ll"},
-        {vowelNbr: 'e', word: "shell", phonetic: "ʃel", end: "ll"},
-        {vowelNbr: 'e', word: "debt", phonetic: "det", end: "bt"},
-        {vowelNbr: 'e', word: "lend", phonetic: "lend", end: "nd"},
-        {vowelNbr: 'e', word: "lent", phonetic: "lent", end: "nt"},
-        {vowelNbr: 'e', word: "send", phonetic: "send", end: "nd"},
-        {vowelNbr: 'e', word: "sense", phonetic: "sens", end: "nse"},
-        {vowelNbr: 'e', word: "guest", phonetic: "gest", end: "st"},
-        {vowelNbr: 'e', word: "self", phonetic: "self", end: "lf"},
-        {vowelNbr: 'e', word: "shelf", phonetic: "ʃelf", end: "lf"},
-        {vowelNbr: 'e', word: "held", phonetic: "held", end: "ld"},
-        {vowelNbr: 'e', word: "best", phonetic: "best", end: "st"},
-        {vowelNbr: 'e', word: "test", phonetic: "test", end: "st"},
-        {vowelNbr: 'e', word: "desk", phonetic: "desk", end: "sk"},
-        {vowelNbr: 'e', word: "dealt", phonetic: "delt", end: "lt"},
-        {vowelNbr: 'e', word: "dent", phonetic: "dent", end: "nt"},
-        {vowelNbr: 'e', word: "bench", phonetic: "bentj", end: "nch"},
-        {vowelNbr: 'e', word: "shelves", phonetic: "ʃelvz", end: "lves"},
-        {vowelNbr: 'e', word: "stair", phonetic: "ster", end: "r"},
-        {vowelNbr: 'e', word: "chair", phonetic: "tjer", end: "r"},
-        {vowelNbr: 'e', word: "prayer", phonetic: "prer", end: "r"}        
+        {vowel: 'e', word: "air", phonetic: "er", end: "r"},
+        {vowel: 'e', word: "heir", phonetic: "er", end: "r"},
+        {vowel: 'e', word: "hair", phonetic: "her", end: "r"},
+        {vowel: 'e', word: "fair", phonetic: "fer", end: "r"},
+        {vowel: 'e', word: "pair", phonetic: "per", end: "r"},
+        {vowel: 'e', word: "bear", phonetic: "ber", end: "r"},
+        {vowel: 'e', word: "care", phonetic: "ker", end: "re"},
+        {vowel: 'e', word: "where", phonetic: "wer", end: "re"},
+        {vowel: 'e', word: "share", phonetic: "ʃer", end: "re"},
+        {vowel: 'e', word: "let", phonetic: "let", end: "t"},
+        {vowel: 'e', word: "less", phonetic: "les", end: "s"},
+        {vowel: 'e', word: "dead", phonetic: "ded", end: "d"},
+        {vowel: 'e', word: "death", phonetic: "deθ", end: "th"},
+        {vowel: 'e', word: "deaf", phonetic: "def", end: "f"},
+        {vowel: 'e', word: "get", phonetic: "get", end: "t"},
+        {vowel: 'e', word: "guess", phonetic: "ges", end: "ss"},
+        {vowel: 'e', word: "pen", phonetic: "pen", end: "n"},
+        {vowel: 'e', word: "bell", phonetic: "bel", end: "ll"},
+        {vowel: 'e', word: "sell", phonetic: "sel", end: "ll"},
+        {vowel: 'e', word: "shell", phonetic: "ʃel", end: "ll"},
+        {vowel: 'e', word: "debt", phonetic: "det", end: "bt"},
+        {vowel: 'e', word: "lend", phonetic: "lend", end: "nd"},
+        {vowel: 'e', word: "lent", phonetic: "lent", end: "nt"},
+        {vowel: 'e', word: "send", phonetic: "send", end: "nd"},
+        {vowel: 'e', word: "sense", phonetic: "sens", end: "nse"},
+        {vowel: 'e', word: "guest", phonetic: "gest", end: "st"},
+        {vowel: 'e', word: "self", phonetic: "self", end: "lf"},
+        {vowel: 'e', word: "shelf", phonetic: "ʃelf", end: "lf"},
+        {vowel: 'e', word: "held", phonetic: "held", end: "ld"},
+        {vowel: 'e', word: "best", phonetic: "best", end: "st"},
+        {vowel: 'e', word: "test", phonetic: "test", end: "st"},
+        {vowel: 'e', word: "desk", phonetic: "desk", end: "sk"},
+        {vowel: 'e', word: "dealt", phonetic: "delt", end: "lt"},
+        {vowel: 'e', word: "dent", phonetic: "dent", end: "nt"},
+        {vowel: 'e', word: "bench", phonetic: "bentj", end: "nch"},
+        {vowel: 'e', word: "shelves", phonetic: "ʃelvz", end: "lves"},
+        {vowel: 'e', word: "stair", phonetic: "ster", end: "r"},
+        {vowel: 'e', word: "chair", phonetic: "tjer", end: "r"},
+        {vowel: 'e', word: "prayer", phonetic: "prer", end: "r"}        
     ]; 
 
-    function getWordsByVowelNbr(vowelNumbers) {
-        return wordBank.filter(item => vowelNumbers.includes(item.vowelNbr));
+    function getWordsByvowel(vowelNumbers) {
+        return wordBank.filter(item => vowelNumbers.includes(item.vowel));
     }
 
     function formatPhoneticText(text) {
@@ -196,89 +196,63 @@ $(document).ready(function(){
     }
     
     function updateInterface(isNext){
-        document.getElementsByClassName("word-container")[0].innerHTML = "";
-        $(".word-container").removeClass("all");
-
-        let selectedVowels = $(".vowel-menu .vowel.active").map(function() {
+        
+        let selectedVowels = $("#filter-buttons button.active").map(function() {
             return $(this).text();
         }).get();
 
         var selectedWordBank = [];
         if (selectedVowels != null && typeof selectedVowels != 'undefined' && selectedVowels.length > 0){
-            selectedWordBank = getWordsByVowelNbr(selectedVowels);
+            selectedWordBank = getWordsByvowel(selectedVowels);
         }
-        else{
-            showAllWords();
-            return;
+        else{            
+            selectedWordBank = wordBank;
         }
 
         if (selectedWordBank == null || typeof selectedWordBank == 'undefined' || selectedWordBank.length == 0){
+            $("#filterable-cards").html("");
             return;
         }
 
-        let item = pickNextWord(isNext, selectedWordBank, true);
-        while (oldWordItem?.word == item?.word){
-            item = pickNextWord(isNext, selectedWordBank, true);
-        }
+        // let item = pickNextWord(isNext, selectedWordBank, true);
+        // while (oldWordItem?.word == item?.word){
+        //     item = pickNextWord(isNext, selectedWordBank, true);
+        // }
 
-        oldWordItem = item;
+        //oldWordItem = item;
 
-        const wordEntryElement = document.createElement(`div`); //document.createElement(`<div class="word-entry"></div>`);
-        wordEntryElement.classList.add("word-entry");
-            
-        wordEntryElement.innerHTML = `
-            <div class="word no_selection">
-                <h3>${boldWordEnd(item)}</h3>
-            </div>
-            <div class="details no_selection">
-                <p class="phonetic"></p>
-            </div>`;
+        $("#filterable-cards").html("");
 
-        wordEntryElement.getElementsByClassName('phonetic')[0].innerHTML = `/ ${formatPhoneticText(item.phonetic)}/`;
-
-        document.getElementsByClassName("word-container")[0].appendChild(wordEntryElement);
-    }
-
-    function showAllWords(){
-        document.getElementsByClassName("word-container")[0].innerHTML = "";
-        $(".word-container").addClass("all");
-        
-        wordBank.forEach((item, idx)=>{
-            const wordEntryElement = document.createElement(`div`); //document.createElement(`<div class="word-entry"></div>`);
-            wordEntryElement.classList.add("word-entry");
-            wordEntryElement.classList.add("all");
-                
-            wordEntryElement.innerHTML = `
-                <div class="word no_selection">
-                    <h3>${boldWordEnd(item)}</h3>
+        selectedWordBank.forEach((item, idx)=>{
+            let wordEntryElement = $(`
+            <div class="card p-0 show" data-name="${item.vowel}">
+                <div class="card-body">
+                    <h2 class="card-title">${boldWordEnd(item)}</h2>
+                    <h4 class="card-text">/ ${formatPhoneticText(item.phonetic)}/</h4>
                 </div>
-                <div class="details no_selection">
-                    <p class="phonetic"></p>
-                </div>`;
+            </div>`);
 
-            wordEntryElement.getElementsByClassName('phonetic')[0].innerHTML = `/ ${formatPhoneticText(item.phonetic)}/`;
-
-            document.getElementsByClassName("word-container")[0].appendChild(wordEntryElement);
+            $("#filterable-cards").append(wordEntryElement);
         });
     }
 
     updateInterface(true);
 
-    $(".vowel-menu .vowel").click(e =>{
+    $("#filter-buttons button").click(e =>{
         $(e.target).toggleClass("active");
         updateInterface(true);
     });
 
-    $(document).on('click', function(e) {
-        var screenWidth = $(window).width();
-        var clickPosition = e.clientX; // Get the horizontal position of the click
+    // $(document).on('click', function(e) {
+    //     var screenWidth = $(window).width();
+    //     var clickPosition = e.clientX; // Get the horizontal position of the click
         
-        if (clickPosition < screenWidth / 2) {
-            // Click was on the left half of the screen
-            updateInterface(false);
-        } else {
-            // Click was on the right half of the screen
-            updateInterface(true);
-        }
-    });
+    //     if (clickPosition < screenWidth / 2) {
+    //         // Click was on the left half of the screen
+    //         updateInterface(false);
+    //     } else {
+    //         // Click was on the right half of the screen
+    //         updateInterface(true);
+    //     }
+    // });
 });
